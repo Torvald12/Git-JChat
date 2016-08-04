90e11aca0f096b639a4c535259c5ad80acb69f28
torvald12 <torvald124@gmail.com>
2016-08-04 16:01:43 +0300

    Базове вікно

---
+++ JChat/src/jchat/JChat.java
@@ -0,0 +1,15 @@
+package jchat;
+
+import javax.swing.JFrame;
+
+public class JChat 
+{
+
+    public static void main(String[] args) 
+    {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(500, 500);
+    }
+    
+}
